76076cc363c47b9fce3caea1bcbc356bd90616c7
Thomas Eding <teding@teding.c.groq-143208.internal>
2019-06-26 15:40:58 -0700

    Added --elipsis option

--- src/Substr.hs
+++ src/Substr.hs
@@ -14,46 +14,57 @@ data Id a = Id a
 
 tryRead :: (Read a) => String -> Maybe a
 tryRead str = case reads str of
-    [(val, "")] -> Just val
-    _ -> Nothing
+  [(val, "")] -> Just val
+  _ -> Nothing
 
 
 parse :: String -> Maybe (Maybe Integer)
 parse str = case tryRead str of
-    Just num -> Just $ Just num
-    Nothing -> case str of
-        "-" -> Just Nothing
-        _ -> Nothing
+  Just num -> Just $ Just num
+  Nothing -> case str of
+    "-" -> Just Nothing
+    _ -> Nothing
 
 
 main :: IO ()
 main = do
-    args <- getArgs
-    range <- case mapM parse args of
-        Just [begin] -> return $ Range begin Nothing
-        Just [begin, end] -> return $ Range begin end
-        _ -> exitFailure
-    interact $ substr range
+  args <- getArgs
+  let (elipsis, args') = case args of
+        "--elipsis" : rest -> (True, rest)
+        _ -> (False, args)
+  range <- case mapM parse args' of
+    Just [begin] -> return $ Range begin Nothing
+    Just [begin, end] -> return $ Range begin end
+    _ -> exitFailure
+  interact $ runProgram elipsis range
+
+
+runProgram :: Bool -> Range Maybe -> String -> String
+runProgram elipsis mRange s = let
+  s' = substr mRange s
+  n' = length s'
+  n  = length s
+  in case elipsis && n' < n of
+    False -> s'
+    True  -> let
+      s'' = take (n' - 3) s'
+      in s'' ++ "..."
 
 
 substr :: Range Maybe -> String -> String
-substr (Range Nothing Nothing) str = str
-substr (Range Nothing end) str = substr (Range (Just 0) end) str
-substr (Range begin Nothing) str = substr (Range begin $ Just $ genericLength str) str
-substr (Range (Just begin) (Just end)) str = substr' (genericLength str) str range'
+substr mRange str = case mRange of
+  Range Nothing Nothing         -> str
+  Range Nothing end             -> substr (Range (Just 0) end) str
+  Range begin Nothing           -> substr (Range begin $ Just $ genericLength str) str
+  Range (Just begin) (Just end) -> substr' (genericLength str) str range'
     where
-        range' = Range (Id begin) (Id end)
+      range' = Range (Id begin) (Id end)
 
 
 substr' :: Integer -> String -> Range Id -> String
 substr' len str (Range (Id begin) (Id end))
-    | begin < 0 = substr' len str $ Range (Id $ len + begin) (Id end)
-    | end < 0 = substr' len str $ Range (Id begin) (Id $ len + end)
-    | otherwise = genericTake (end - begin) . genericDrop begin $ str
-    
-
-
-
-
+  | begin < 0 = substr' len str $ Range (Id $ len + begin) (Id end)
+  | end < 0 = substr' len str $ Range (Id begin) (Id $ len + end)
+  | otherwise = genericTake (end - begin) . genericDrop begin $ str
 
 
